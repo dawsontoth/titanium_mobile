4aeffab8b386b9ff39ca355d5deee3c5580e1df2
Dawson Toth <dtoth@appcelerator.com>
2012-03-05 10:28:20 -0600

    titanium install > open Xcode

--- support/module/builder.py
+++ support/module/builder.py
@@ -213,14 +213,22 @@ def main(args):
 		stage(platform, project_dir, manifest, run_callback)
 	elif command == 'install':
 		def install_callback(gen_project_dir):
-
+			script = os.path.abspath(os.path.join(template_dir, '..', platform, 'builder.py'))
+			
 			if is_ios(platform):
-				print "[ERROR] Build Failed. Install module to device not supported on iOS."
+				script_args = [script, 'run', gen_project_dir]
+				rc = run_python(script_args)
+				if rc != 0:
+					if is_ios(platform):
+						error = os.path.join(gen_project_dir, 'build', 'iphone', 'build', 'build.log')
+						print "[ERROR] Build Failed. See: %s" % os.path.abspath(error)
+					else:
+						print "[ERROR] Build Failed."
+				run(['open', os.path.join(gen_project_dir, 'build', 'iphone', manifest.name + '.xcodeproj')], cwd=gen_project_dir)
 				return
 
 			tiapp_xml = os.path.join(gen_project_dir, 'tiapp.xml')
 
-			script = os.path.abspath(os.path.join(template_dir, '..', platform, 'builder.py'))
 			script_args = [script, "install", manifest.name, android_sdk.get_android_sdk(), gen_project_dir, manifest.moduleid, "Necessary argument, but unused."]
 
 			rc = run_python(script_args)
