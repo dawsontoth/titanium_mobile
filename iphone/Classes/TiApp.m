8e6a1a479f729b89eeb9c06530d9342b3d2c97ed
Dawson Toth <dtoth@appcelerator.com>
2013-07-26 13:34:12 -0500

    Expand Error Properties

--- iphone/Classes/TiApp.m
+++ iphone/Classes/TiApp.m
@@ -571,14 +571,6 @@ TI_INLINE void waitForMemoryPanicCleared();   //WARNING: This must never be run 
 //TODO: this should be compiled out in production mode
 -(void)showModalError:(NSString*)message
 {
-	if ([TI_APPLICATION_DEPLOYTYPE isEqualToString:@"production"])
-	{
-		NSLog(@"[ERROR] Application received error: %@",message);
-		return;
-	}
-	ENSURE_UI_THREAD(showModalError,message);
-	TiErrorController *error = [[[TiErrorController alloc] initWithError:message] autorelease];
-	[controller presentModalViewController:error animated:YES];
 }
 
 -(void)attachModal:(UIViewController*)modalController toController:(UIViewController*)presentingController animated:(BOOL)animated
